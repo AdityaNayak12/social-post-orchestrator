44c67a2c44b92d2962ad951316f405c753042f24
AdityaNayak12 <adinayak18@gmail.com>
2026-03-20 14:46:32 +0530

    fix(config): fixed to get sheet client credentials

--- app/config.py
+++ app/config.py
@@ -5,9 +5,14 @@ load_dotenv()
 
 
 class Settings:
-    INTERNAL_TOKEN: str = os.getenv("INTERNAL_TOKEN")
+    INTERNAL_TOKEN: str = os.getenv("INTERNAL_TOKEN") or ""
 
-    if INTERNAL_TOKEN is None:
+    GOOGLE_SERVICE_ACCOUNT_EMAIL: str = os.getenv("GOOGLE_SERVICE_ACCOUNT_EMAIL") or ""
+    GOOGLE_PRIVATE_KEY: str = os.getenv("GOOGLE_PRIVATE_KEY") or ""
+    GOOGLE_SPREADSHEET_ID: str = os.getenv("GOOGLE_SPREADSHEET_ID") or ""
+    GOOGLE_SHEET_NAME: str = os.getenv("GOOGLE_SHEET_NAME") or "Sheet1"
+
+    if not INTERNAL_TOKEN
         raise ValueError("INTERNAL_TOKEN must be set in environment variables")
 
 
